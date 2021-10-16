3bd450cd8e03bff7be6768e5d9c65d16ce50d36b
chenfengjin <1871653365@qq.com>
2021-10-17 01:42:38 +0800

    backup for eth-proxy and a log of things

---
+++ bcs/contract/ethproxy_test.go
@@ -0,0 +1,77 @@
+package contract
+
+import (
+	"encoding/json"
+	log15 "github.com/xuperchain/log15"
+	_ "github.com/xuperchain/xupercore/bcs/contract/evm"
+	"github.com/xuperchain/xupercore/kernel/contract"
+	"github.com/xuperchain/xupercore/kernel/contract/mock"
+	"io/ioutil"
+	"testing"
+)
+
+type evmtransaction struct {
+}
+
+func TestEVM(t *testing.T) {
+	var logger = log15.New()
+	var contractConfig = &contract.ContractConfig{
+		EnableUpgrade: true,
+		Xkernel: contract.XkernelConfig{
+			Enable: true,
+			Driver: "default",
+		},
+		Native: contract.NativeConfig{
+			Enable: true,
+			Driver: "native",
+		},
+		EVM: contract.EVMConfig{
+			Enable: true,
+			Driver: "evm",
+		},
+		LogDriver: &MockLogger{
+			logger,
+		},
+	}
+	th := mock.NewTestHelper(contractConfig)
+	defer th.Close()
+	//m := th.Manager()
+
+	//m.GetKernRegistry().RegisterKernMethod("$evm", "proxy", new(helloContract).Hi)
+	txData := evmtransaction{}
+	data, err := json.Marshal(txData)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	//prepare env
+	bin, err := ioutil.ReadFile("testdata/counter.bin")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	abi, err := ioutil.ReadFile("testdata/counter.abi")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	args := map[string][]byte{
+		"contract_abi": abi,
+		"input":        bin,
+		"jsonEncoded":  []byte("false"),
+	}
+	resp, err := th.Deploy("evm", "counter", "counter", bin, args)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// unit test
+	resp, err = th.Invoke("xkernel", "$evm", "proxy", map[string][]byte{
+		"desc": data,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("%s", resp.Body)
+}
