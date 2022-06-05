27e2bcdb9ebede408727bb319e718ca5005c2e5f
chenfengjin <chenfengjin@baidu.com>
2022-06-06 07:43:13 +0800

    add ewasm poc

---
+++ bcs/contract/evm/ewasm.go
@@ -0,0 +1,24 @@
+package evm
+
+// func newEWASMCreator(config *bridge.InstanceCreatorConfig) (bridge.InstanceCreator, error) {
+// 	// opt := evm.Options{}
+// 	// vm := evm.New(opt)
+// 	return &eWASMCreator{
+// 		// vm: vm,
+// 	}, nil
+// }
+//
+// type eWASMCreator struct {
+// }
+//
+// func (c *eWASMCreator) CreateInstance(ctx *bridge.Context, cp bridge.ContractCodeProvider) (bridge.Instance, error) {
+// 	// TODO
+// 	return nil, nil
+// }
+// func (c *eWASMCreator) RemoveCache(name string) {
+//
+// }
+//
+// func init() {
+// 	bridge.Register(bridge.TypeEvm, "ewasm", newEWASMCreator)
+// }
